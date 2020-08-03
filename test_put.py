7923be88a7dd25d57c333fa0362953be33506047
ChataingT <38420068+ChataingT@users.noreply.github.com>
2020-08-03 15:21:30 +0200

    add predict data

--- test_put.py
+++ test_put.py
@@ -8,7 +8,7 @@ Created on Mon Aug  3 12:36:26 2020
 import requests
 
 response = requests.put('http://127.0.0.1:5000/api/countryByDate', json={
-        "country": "tot",
+        "country": "aPlace",
         "date": "8/3/20",
         "cases" : 1,
         "deaths" : 3,
